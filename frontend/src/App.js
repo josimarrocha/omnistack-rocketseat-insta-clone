fa82df6912eecf23e5b579b78c176de75372bc4d
Josimar <rocha_josimar@yahoo.com.br>
2019-06-25 13:09:12 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Header from './components/Header'
+import Routes from './routes'
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
